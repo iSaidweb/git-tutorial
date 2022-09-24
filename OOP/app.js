64a575ed6fa0526d2e7414044cb45dc765470d01
iSaidWeb <devsaidislom@gmail.com>
2022-09-24 12:25:30 +0500

    OOP

---
+++ OOP/app.js
@@ -0,0 +1,19 @@
+"use strict";
+const car = {
+    motor: "200XZT",
+    color: "red",
+    isAirBag: true,
+};
+const gmCar = {
+    isAirBag: false
+};
+// gmCar.__proto__ = car;  eski versiya
+Object.setPrototypeOf(gmCar, car); //yangi
+const newCar = Object.create(car);//Boshqa yo'li
+
+console.log(car);
+console.log(gmCar);
+console.log(gmCar.color);
+//qisqacha qilib prototype yaratish || prototype ga yo'naltirilgan meros
+
+const a = () =>{};
